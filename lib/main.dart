f377639a22407f6b31a8776be7ea62db8e97c81c
Thibault Magnini <thibault.magnini@student.ucll.be>
2021-06-15 21:26:16 +0200

    extra information tab with daily numbers on home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'dart:convert';
 
 import 'package:covid_app/constant.dart';
-import 'package:covid_app/widgets/line_chart.dart';
 import 'package:covid_app/widgets/navigation_drawer_widget.dart';
-import 'package:fl_animated_linechart/chart/area_line_chart.dart';
-import 'package:fl_animated_linechart/common/pair.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fl_animated_linechart/fl_animated_linechart.dart';
-//import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 
@@ -42,6 +38,23 @@ class HomePage extends State<Home> {
   var scaffoldKey = GlobalKey<ScaffoldState>();
   String selectedCat = "Active Infections";
   int chartIndex = 1;
+  String infectedToday = "NA";
+  String recoveredToday = "NA";
+  String deathsToday = "NA";
+
+  HomePage() {
+    _fetchLatestInfection().then((val) => setState(() {
+          infectedToday = val;
+        }));
+
+    _fetchLatestDeaths().then((val) => setState(() {
+          deathsToday = val;
+        }));
+
+    _fetchLatestRecovered().then((val) => setState(() {
+          recoveredToday = val;
+        }));
+  }
 
   LineChart chart;
   DateFormat formatter = DateFormat('dd-MM-yyyy');
@@ -98,7 +111,7 @@ class HomePage extends State<Home> {
                             Positioned(
                               left: 160,
                               child: Text(
-                                "CoronaPredBE \nCovid-19 forecasts.",
+                                "CoronaPredBE \nCovid-19 forecast",
                                 style: kHeadingTextStyle.copyWith(
                                     color: Colors.white),
                               ),
@@ -185,8 +198,49 @@ class HomePage extends State<Home> {
               ),
               Container(
                 margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 4),
+                      blurRadius: 30,
+                      color: kShadowColor,
+                    ),
+                  ],
+                  border: Border.all(
+                    color: Color(0xFFE5E5E5),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Counter(
+                      number: infectedToday,
+                      title: "infections",
+                      color: kInfectedColor,
+                    ),
+                    Counter(
+                      number: recoveredToday,
+                      title: "recoveries",
+                      color: kRecovercolor,
+                    ),
+                    Counter(
+                      number: deathsToday,
+                      title: "deaths",
+                      color: kDeathColor,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
                 padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-                height: 310,
+                height: 350,
                 width: double.infinity,
                 decoration: BoxDecoration(
                   color: Colors.white,
@@ -222,7 +276,11 @@ class HomePage extends State<Home> {
                       ], tapTextFontWeight: FontWeight.w400);
                       return new AnimatedLineChart(chart);
                     } else {
-                      return CircularProgressIndicator();
+                      return SizedBox(
+                        child: CircularProgressIndicator(),
+                        height: 20,
+                        width: 20,
+                      );
                     }
                   },
                 ),
@@ -236,24 +294,94 @@ class HomePage extends State<Home> {
       );
 }
 
+class Counter extends StatelessWidget {
+  const Counter({
+    Key key,
+    @required this.number,
+    @required this.title,
+    @required this.color,
+  }) : super(key: key);
+
+  final String number;
+  final Color color;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(6),
+          height: 25,
+          width: 25,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color.withOpacity(.26),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.transparent,
+              border: Border.all(color: color, width: 2),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          number,
+          style: TextStyle(fontSize: 40, color: color),
+        ),
+        Text(
+          title,
+          style: kSubTextStyle,
+        )
+      ],
+    );
+  }
+}
+
+Future<String> _fetchLatestInfection() async {
+  var response =
+      await http.get(Uri.parse("http://139.162.248.210:8000/cases/belgium"));
+  if (response.statusCode == 200) {
+    final responseJson = jsonDecode(response.body);
+    print(responseJson[responseJson.length - 1]["NEW_CASES"]);
+    return responseJson[responseJson.length - 3]["NEW_CASES"];
+  } else {
+    return "NA";
+  }
+}
+
+Future<String> _fetchLatestDeaths() async {
+  var response =
+      await http.get(Uri.parse("http://139.162.248.210:8000/cases/belgium"));
+  if (response.statusCode == 200) {
+    final responseJson = jsonDecode(response.body);
+    return responseJson[responseJson.length - 3]["NEW_DEATHS"];
+  } else {
+    return "NA";
+  }
+}
+
+Future<String> _fetchLatestRecovered() async {
+  var response =
+      await http.get(Uri.parse("http://139.162.248.210:8000/cases/belgium"));
+  if (response.statusCode == 200) {
+    final responseJson = jsonDecode(response.body);
+    return responseJson[responseJson.length - 3]["NEW_RECOVERED"];
+  } else {
+    return "NA";
+  }
+}
+
 Future<Map<DateTime, double>> _fetchCovidData(int chartIndex) async {
   Map<DateTime, double> data = {};
   var response;
-  if (chartIndex == 1) {
-    response = await http.get(Uri.parse("http://10.0.2.2:8000/cases/belgium"));
-    if (response.statusCode == 200) {
-      final responseJson = jsonDecode(response.body);
-      for (int i = responseJson.length - 3; i > responseJson.length - 33; i--) {
-        data[DateTime.parse(responseJson[i]["DATE"])] =
-            double.parse(responseJson[i]["ACTIVE_CASES"]);
-      }
-    } else {
-      throw Exception("Failed to load data.");
-    }
-    return data;
-  } else if (chartIndex == 3) {
+  if (chartIndex == 3) {
     response = await http
-        .get(Uri.parse("http://10.0.2.2:8000/hospitalisations/belgium"));
+        .get(Uri.parse("http://139.162.248.210:8000/hospitalisations/belgium"));
     if (response.statusCode == 200) {
       final responseJson = jsonDecode(response.body);
       for (int i = responseJson.length - 3; i > responseJson.length - 33; i--) {
@@ -265,7 +393,8 @@ Future<Map<DateTime, double>> _fetchCovidData(int chartIndex) async {
     }
     return data;
   } else if (chartIndex == 2) {
-    response = await http.get(Uri.parse("http://10.0.2.2:8000/cases/belgium"));
+    response =
+        await http.get(Uri.parse("http://139.162.248.210:8000/cases/belgium"));
     if (response.statusCode == 200) {
       final responseJson = jsonDecode(response.body);
       for (int i = responseJson.length - 3; i > responseJson.length - 33; i--) {
@@ -277,7 +406,8 @@ Future<Map<DateTime, double>> _fetchCovidData(int chartIndex) async {
     }
     return data;
   } else if (chartIndex == 4) {
-    response = await http.get(Uri.parse("http://10.0.2.2:8000/cases/belgium"));
+    response =
+        await http.get(Uri.parse("http://139.162.248.210:8000/cases/belgium"));
     if (response.statusCode == 200) {
       final responseJson = jsonDecode(response.body);
       for (int i = responseJson.length - 3; i > responseJson.length - 33; i--) {
@@ -289,7 +419,8 @@ Future<Map<DateTime, double>> _fetchCovidData(int chartIndex) async {
     }
     return data;
   } else {
-    response = await http.get(Uri.parse("http://10.0.2.2:8000/cases/belgium"));
+    response =
+        await http.get(Uri.parse("http://139.162.248.210:8000/cases/belgium"));
     if (response.statusCode == 200) {
       final responseJson = jsonDecode(response.body);
       for (int i = responseJson.length - 3; i > responseJson.length - 33; i--) {
